33a4b7d0030675f6653f7246e22a3c3f16ae429e
Marco B <m.brockmann@reanmo.com>
2022-03-25 10:06:32 +0100

    Added links

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,9 +23,16 @@ class LoginController extends Controller
         $user = auth()->user();
         $token = $user->createToken('authenticationToken')->plainTextToken;
 
+        $uuid = $user->getAttribute('uuid');
+
         $response = [
             'user' => UserResource::make($user),
-            'token' => $token
+            'token' => $token,
+            'links' => [
+                'home' => 'api/tweets',
+                'myTweets' => 'api/tweets?user=' . $uuid,
+                'settings' => 'api/users/' . $uuid
+            ]
         ];
 
         return response()->json($response, Response::HTTP_OK);
