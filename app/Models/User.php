6cef943d22a66f308e34d8897c9d5b20008f9eba
Marco B <m.brockmann@reanmo.com>
2022-03-28 10:22:09 +0200

    Changed RouteKeyName to uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,14 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Image::class, 'img_ID');
     }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }
