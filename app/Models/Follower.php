ce05ab1cc587687eae8898559abd75b0eaed694b
Marco B <m.brockmann@reanmo.com>
2022-04-04 12:40:20 +0200

    Created Follower Model and Migration

---
+++ app/Models/Follower.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Follower extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['follower_id', 'followed_id'];
+
+    public function followers() {
+        $this->hasMany('users', 'id', 'followed_id');
+    }
+
+    public function followedBy() {
+        $this->belongsTo('users', 'id', 'follower_id');
+    }
+}
