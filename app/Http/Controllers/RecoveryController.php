90491839eef43ef6ba345385b368436b9956533c
Marco B <m.brockmann@reanmo.com>
2022-04-08 09:30:32 +0200

    Implemented RecoveryRequest and adjusted Controller

--- app/Http/Controllers/RecoveryController.php
+++ app/Http/Controllers/RecoveryController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RecoveryRequest;
 use App\Models\User;
 use App\Services\Recovery;
 use Symfony\Component\HttpFoundation\Response;
 
 class RecoveryController extends Controller
 {
-    public function update(User $user) {
+    public function update(RecoveryRequest $request) {
+        $attributes = $request->validated();
+        $user = User::firstWhere('email', '=', $attributes['email']);
         $service = new Recovery();
         $service->handle($user);
-        //return redirect('/api/logout');
         return response()->json('password-reset', Response::HTTP_CREATED);
     }
 }
