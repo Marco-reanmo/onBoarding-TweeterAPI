a65c390a302c17fe9040a7d1cfcd216adc5924c6
Marco B <m.brockmann@reanmo.com>
2022-04-08 12:32:51 +0200

    Used __invoke-Method for only Function in Recovery-Service

--- app/Http/Controllers/RecoveryController.php
+++ app/Http/Controllers/RecoveryController.php
@@ -12,8 +12,7 @@ class RecoveryController extends Controller
     public function update(RecoveryRequest $request) {
         $attributes = $request->validated();
         $user = User::firstWhere('email', '=', $attributes['email']);
-        $service = new Recovery();
-        $service->handle($user);
+        (new Recovery)($user);
         return response()->json('password-reset', Response::HTTP_CREATED);
     }
 }
--- app/Services/Recovery.php
+++ app/Services/Recovery.php
@@ -2,16 +2,13 @@
 
 namespace App\Services;
 
-use App\Http\Resources\UserResource;
 use App\Jobs\SendRecoveryEmail;
-use App\Jobs\SendVerificationEmail;
 use App\Models\User;
-use App\Models\VerificationToken;
 use Illuminate\Support\Str;
 
 class Recovery
 {
-    public function handle(User $user) {
+    public function __invoke(User $user) {
         $generatedPassword = Str::random(8);
         $encryptedNewPassword = bcrypt($generatedPassword);
         $user->update(['password' => $encryptedNewPassword]);
