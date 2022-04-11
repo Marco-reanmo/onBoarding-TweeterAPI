8ef6feef5244f8a61d7d360899266c73f6b2eac6
Marco B <m.brockmann@reanmo.com>
2022-04-11 11:17:19 +0200

    Added return-types

--- app/Http/Requests/RecoveryRequest.php
+++ app/Http/Requests/RecoveryRequest.php
@@ -12,7 +12,7 @@ class RecoveryRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -22,7 +22,7 @@ class RecoveryRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'email' => ['required', Rule::exists('Users', 'email')]
--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -13,7 +13,7 @@ class UpdateUserRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -23,7 +23,7 @@ class UpdateUserRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'forename' => ['min:3', 'max:255', 'alpha'],
