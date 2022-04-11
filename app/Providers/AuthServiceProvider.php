b42548d0e78f0154ea33c2ad525b13b3801dd6c5
Marco B <m.brockmann@reanmo.com>
2022-04-11 11:25:59 +0200

    Removed unused import

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use App\Policies\TweetPolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rules\Password;
 
 class AuthServiceProvider extends ServiceProvider
