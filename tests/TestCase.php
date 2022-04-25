a1743fffeba31115b52ebc100f02c5f880d87259
Marco B <m.brockmann@reanmo.com>
2022-04-25 07:46:20 +0200

    Removed unnecessary imports

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,10 @@
 
 namespace Tests;
 
-use Exception;
 use Faker\Factory;
 use Faker\Generator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
